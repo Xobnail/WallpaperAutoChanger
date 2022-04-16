e7f00752e458d06cfaffd5d042022091de497d06
Nail Khabibullin <xobnail@gmail.com>
2022-04-16 14:31:32 +0300

    EF models were scaffolded

---
+++ Wallpaper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WallpaperAutoChanger
+{
+    public partial class Wallpaper
+    {
+        public int Id { get; set; }
+        public string? Url { get; set; }
+    }
+}
---
+++ wallpapersdbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WallpaperAutoChanger
+{
+    public partial class wallpapersdbContext : DbContext
+    {
+        public wallpapersdbContext()
+        {
+        }
+
+        public wallpapersdbContext(DbContextOptions<wallpapersdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Wallpaper> Wallpapers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost:5432;Database=wallpapersdb;Username=xobnail;Password=0000");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Wallpaper>(entity =>
+            {
+                entity.ToTable("wallpaper");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Url)
+                    .HasMaxLength(255)
+                    .HasColumnName("url");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
